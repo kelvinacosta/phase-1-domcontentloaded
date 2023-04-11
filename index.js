c32d32b5314c5d8591c88f5980174c7a366e5504
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-04-10 22:53:28 -0400

    complete DOM content loaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+//Create a Dom content 
+document.addEventListener("DOMContentLoaded",function(){
+    //Change the text of p
+    let text = document.querySelector('p')
+    text.textContent = 'This is really cool!'
+})
